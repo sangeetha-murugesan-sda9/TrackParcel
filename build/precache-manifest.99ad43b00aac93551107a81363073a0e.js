a06ef671eb6ee3e746d09b08acc90b8eceef482a
sangeetha-murugesan-sda9 <geethastockholm@gmail.com>
2021-04-06 18:46:55 +0200

    Added FireBase

---
+++ build/precache-manifest.99ad43b00aac93551107a81363073a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ae9253e3e3b8eee33129879ccaf9db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba44f42dc2fa75aa2c5e",
+    "url": "/static/css/main.f546b1fe.chunk.css"
+  },
+  {
+    "revision": "bea608dc2bd5e1b405d7",
+    "url": "/static/js/2.c5893d48.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c5893d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba44f42dc2fa75aa2c5e",
+    "url": "/static/js/main.70f10746.chunk.js"
+  },
+  {
+    "revision": "88199dbb8b3033382a04",
+    "url": "/static/js/runtime-main.40c5eec3.js"
+  }
+]);
--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -20,36 +20,52 @@ export default function DisplayOrders(props) {
         <h4>📦 Parcel Express </h4>
         <label>📦 Order Details</label>
       </div>
-      <table id="customers">
-        <tr>
-          <th>Serial Number</th>
-          <th>Parcel Id</th>
-          <th>Arrival Date</th>
-          <th>Sender</th>
-          <th> Location Id</th>
-          <th> Location Name</th>
-          <th> User Name</th>
-          <th> User PhoneNumber</th>
-          <th> ETA</th>
-          <th> Delivery Status</th>
-        </tr>
-        {listData !== undefined ? (
+      {listData !== undefined ? (
+        <table id="customers">
           <tr>
+            <th>Serial Number</th>
             <td>1</td>
+          </tr>
+          <tr>
+            <th>Parcel Id</th>
             <td>{listData.parcel_id}</td>
+          </tr>
+          <tr>
+            <th>Arrival Date</th>
             <td>{moment(listData.last_updated).format("MM/DD/YYYY")}</td>
+          </tr>
+          <tr>
+            <th>Sender</th>
             <td>{listData.sender}</td>
+          </tr>
+          <tr>
+            <th> Location Id</th>
             <td>{listData.location_id}</td>
+          </tr>
+          <tr>
+            <th> Location Name</th>
             <td>{listData.location_name}</td>
+          </tr>
+          <tr>
+            <th> User Name</th>
             <td>{listData.user_name}</td>
+          </tr>
+          <tr>
+            <th> User PhoneNumber</th>
             <td>{listData.user_phone}</td>
+          </tr>
+          <tr>
+            <th> ETA</th>
             <td>{moment(listData.eta).format("MM/DD/YYYY")}</td>
+          </tr>
+          <tr>
+            <th> Delivery Status</th>
             <td>{listData.status}</td>
           </tr>
-        ) : (
-          []
-        )}
-      </table>
+        </table>
+      ) : (
+        []
+      )}
     </div>
   );
 }
